722b453d4fb72ea4560f85fd705bf4b1e477d69d
cwu67414 <cwu67414@usc.edu>
2024-04-03 14:59:42 -0700

    3rd

--- camera.py
+++ camera.py
@@ -1,7 +1,7 @@
 import cv2
 #print(cv2.__version__)
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink")
 
 while True:
     # Capture frames from the left and right cameras
@@ -11,7 +11,7 @@ while True:
     
 
     # Display the resulting frame
-    cv2.imshow('Camera Feed', frame)
+    cv2.imshow('Frame', frame)
 
     # Break the loop on 'q' key press
     if cv2.waitKey(1) & 0xFF == ord('q'):
