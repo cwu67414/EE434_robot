749f8e97cd1a4c3e51bb1550012b47acfa52f7ad
cwu67414 <cwu67414@usc.edu>
2024-04-03 14:43:43 -0700

    2nd

--- camera.py
+++ camera.py
@@ -1,2 +1,22 @@
 import cv2
-print(cv2.__version__)
+#print(cv2.__version__)
+
+cap = cv2.VideoCapture("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink")
+
+while True:
+    # Capture frames from the left and right cameras
+    ret, frame = cap.read()
+
+    # Perform stereo vision processing here if needed
+    
+
+    # Display the resulting frame
+    cv2.imshow('Frame', frame)
+
+    # Break the loop on 'q' key press
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the camera resources
+cap.release()
+cv2.destroyAllWindows()
