f02dac66476bb9b05b6e824da5c6eaef89ec7d16
cwu67414 <cwu67414@usc.edu>
2024-04-06 20:10:06 -0700

    4th

--- camera.py
+++ camera.py
@@ -4,6 +4,7 @@ import cv2
 cap = cv2.VideoCapture("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink")
 
 while True:
+    
     # Capture frame-by-frame
     ret, frame = cap.read()
 
