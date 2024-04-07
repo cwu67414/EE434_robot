623fe91da01b69e96464c333ce3e35d00f29cd6e
cwu67414 <cwu67414@usc.edu>
2024-04-06 19:34:41 -0700

    4th

--- camera.py
+++ camera.py
@@ -6,6 +6,6 @@ for index in range(4):  # Try indices from 0 to 3 (or higher if needed)
         print(f"Camera {index} not available")
     else:
         print(f"Camera {index} is available")
-        cap.release()  # Release the camera
-        break  # Exit loop if a camera is found
+cap.release()  # Release the camera
+break  # Exit loop if a camera is found
     
